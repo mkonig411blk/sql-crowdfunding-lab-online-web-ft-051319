5e2fbaedac3e0880e2f4924cf05dfb028de3b605
Margot Konig <margot.konig@blackrock.com>
2019-06-10 22:26:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO projects (cat_id, owner_id) VALUES (3, 2);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (3, 2);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (3, 3);
 INSERT INTO users (cat_id, owner_id) VALUES (1, 2);
