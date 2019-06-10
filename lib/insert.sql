9c08a0a0185c1db31c2d0ce8f26f0b6488e42478
Margot Konig <margot.konig@blackrock.com>
2019-06-10 22:28:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,9 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (Test, Test, 100, Jan_5, Jan_10);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (Test, Test, 100, Jan_5, Jan_10);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (Test, Test, 100, Jan_5, Jan_10);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (Test, Test, 100, Jan_5, Jan_10);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (Test, Test, 100, Jan_5, Jan_10);
+
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
