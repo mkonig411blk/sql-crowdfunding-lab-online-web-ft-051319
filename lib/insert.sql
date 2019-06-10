053ca7c1c351965886ca1047572f2c2f593b039e
Margot Konig <margot.konig@blackrock.com>
2019-06-10 22:30:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,6 +8,16 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Test", "Test", 100, "Jan_5", "Jan_10");
 
 
 
@@ -35,23 +45,4 @@ INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
 INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
-INSERT INTO users (name, age) VALUES (Adam, 27);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
-INSERT INTO users (name, age) VALUES (Adam, 27);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
-INSERT INTO users (name, age) VALUES (Adam, 27);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
-INSERT INTO users (name, age) VALUES (Adam, 27);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
-INSERT INTO users (name, age) VALUES (Adam, 27);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
-INSERT INTO users (name, age) VALUES (Adam, 27);
-INSERT INTO users (name, age) VALUES (Margot, 27);
-INSERT INTO users (name, age) VALUES (Brian, 27);
+INSERT INTO users (name, age) VALUES ("Margot", 27);
